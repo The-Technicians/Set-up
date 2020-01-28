7a21cccdde186e88fb13d67c4b28c9414ee7c366
GpXavier <59236910+Xstudios2019@users.noreply.github.com>
2020-01-28 01:10:21 -0600

    Update OS.bat

--- OS/OS.bat
+++ OS/OS.bat
@@ -1,110 +1,81 @@
-@echo off
-color f2
+ @echo off
+color 0a
+title Purnjay OS ver= 0.5.1
+echo Finding Kernel.........
+echo Kernel Found! Booting PurnjayOS
+pause
 
-mode 300
-
-title operating system
+cls            
+echo what is your name?
+set/p name=
+echo.
+cls
+echo HELLO! %name%
+echo.
+pause
 
 :menu
-
+cls
+echo What Would You Like To Do? Type the number.
 echo.
+echo 1.System Info 2.Calc 3.Clock 4.Calendar 5.Songs 6.Antivirus
 
-echo.
+set /p input=
+if %input% == 1 goto info
+if %input% == 2 goto calc 
+if %input% == 3 goto clock
+if %input% == 4 goto calendar
+if %input% == 5 goto Songs
+if %input% == 6 goto antivirus
 
-echo ------------
-
-echo hello
-
-echo %username%
-
-echo operating
-
-echo system(os)
-
-echo ------------
-
-echo.
-
-echo --------------------------
-
-echo type 1 for cmd
-
-echo type 2 for matrix
-
-echo type 3 for rainbow matrix
-
-echo type 4 for notepad
-
-echo type 5 for mini os
-
-echo type 6 for youtube
-
-echo type 7 for google
-
-echo type 8 for instructables
-
-echo type 9 for happyhweels
-
-echo type 10 for searchy engine
-
-echo -------------------------
-
-set /p input=cmd:
-
-if %input%==1 goto cmd
-
-if %input%==2 goto matrix
-
-if %input%==3 goto rainbow matrix
-
-if %input%==5 goto mini os
-
-if %input%==4 start notepad
-
-if %input%==6 start www.youtube.com
-
-if %input%==7 start www.youtube.com
-
-if %input%==8 start www.youtube.com
-
-if %input%==9 start www.youtube.com
-
-if %input%==10 goto BROWSER
+:info
 
 cls
-
-:cmd
-
-title Command Prompt - by account3r2
-
-ver
-
-echo (C) Copyright Microsoft Corp.
-
-echo (type "poopslap" to go back.)
-
+echo.=======================
+echo     PURNJAY__OS_
+echo.=======================
+echo     DETAILS  
 echo.
-
-set /p "cmd=%cd%>"
-
-if %cmd%==poopslap goto menu
-
-%cmd%
-
+echo     VERSION = 0.0.1
+echo.     
+echo     RAM = 1GB
 echo.
+echo     CORE = INTEL I5
+echo.
+echo     HARD_DRIVE = 250GB     
+echo.   
+echo     Kernel ver = 5.6.7
+echo.  
+echo    Build = 1
+echo.
+echo.========================
+echo     @Copyright P.inc
+echo.========================
+echo.
+echo Wanna check for update?
 
-goto cmd
+echo  1. to check for update 
+echo  2. Menu
 
-:matrix
+set /p check= 
 
-echo %random% %random% %random% %random% %random% %random% %random% %random% r%andom% %random% %random% %random% %random% %random% goto matrix :rainbow matrix color a1 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% color b2 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% color c3 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% color d4 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% color e5 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% color f6 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% goto rainbow matrix
+if %check% == 1 goto update
 
-cls :BROWSER color f3 set /p input=search:
+if %check% == 2 goto menu
 
-if %input%==google start www.google.com if %input%==bing start www.google.com if %Input%==yahoo start www.google.com if %input%==instructables start www.google.com if %Input%==facebook start www.google.com if %input%==instagram start www.google.com if %input%==youtube start www.google.com %input% echo. goto BROWSER
 
-:mini os @echo off color f2 title INFINITE OS 2 dev stage :Main echo. echo. echo ------------------------------ echo infinite os 2 dev stage echo -------------------------------- echo The mini OS IN BATCH IS BACK echo. echo ------------------ echo Type 1 for info echo Type 2 for matrix echo type 3 for mini os echo type 4 for youtube echo type 5 to restart echo type 6 to go back echo to super os. echo Type 10 for cmd echo ---------------------- set /p input=COMMAND? if %input%==1 goto INFO if %input%==2 goto Matrix if %input%==10 goto cmd if %input%==3 goto mini os if %input%==4 start www.youtube.com if %input%==5 goto main if %input%==6 goto menu :INFO echo this is the dev stage of the infinite os 2 ver 0.1 echo the last os was never recoverd from the last flash drive echo type 1 to go back set /p input=COMMAND? if %input%==1 goto Main cls
 
-:Matrix @echo off title Matrix mode 300 :start COLOR 4 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% goto start
 
-cls :cmd set /p cmd=C:\Enter Command: %cmd% echo. goto cmd
+     
+:update 
+cls
+
+echo Checking for new version /Update.............
+echo.
+pause
+cls
+echo.
+echo no update available,Try Againg after some time.
+echo.
+pause
+goto menu
